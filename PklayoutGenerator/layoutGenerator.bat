bcac998d1bdd9199d629ff478437ea3fd9a08317
Alvin Yee <changchang.ye@hotmail.com>
2019-05-27 11:29:44 +0800

    Update layoutGenerator.bat

--- PklayoutGenerator/layoutGenerator.bat
+++ PklayoutGenerator/layoutGenerator.bat
@@ -4,15 +4,23 @@ echo [INFO]: dbcModel.layoutGenerator starts
 echo [version]: 1.0
 echo ************************************************************************
 
+@echo [HINT] must change the name of dbc to ESCL.dbc
 set dbc=..\dbc\ESCL.dbc
+
+@echo [HINT] must change the name to ESCL/ESL...
+set ESCL_NAME=ESL
+
+@echo [HINT] must change the msg list to Coco
+set RxMsgList=
+
 set I2CCAN_par_h=..\output\I2CCAN_Par.h
 set I2CCAN_par_c=..\output\I2CCAN_Par.c
-set ESCL_NAME= ESL
+
 if not exist %dbc% goto ErrorWrongPath
 if not exist %I2CCAN_par_h% cd.> %I2CCAN_par_h%
 if not exist %I2CCAN_par_c% cd.> %I2CCAN_par_c%
 
-python MdlayoutGenerator.py %dbc% %I2CCAN_par_h% %I2CCAN_par_c% %ESCL_NAME%
+python MdlayoutGenerator.py %dbc% %I2CCAN_par_h% %I2CCAN_par_c% %ESCL_NAME% %RxMsgList%
 
 goto End
 
